39825b64b960032a0c478523a273f0b74064faee
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-11 23:54:48 +0600

    Create authorization with mock database

--- src/main/java/kz/zhelezyaka/repositories/UsersRepositoryInMemoryImpl.java
+++ src/main/java/kz/zhelezyaka/repositories/UsersRepositoryInMemoryImpl.java
@@ -1,32 +1,30 @@
 package kz.zhelezyaka.repositories;
 
+import kz.zhelezyaka.mock.MockStorage;
 import kz.zhelezyaka.models.User;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UsersRepositoryInMemoryImpl implements UsersRepository {
-    private List<User> users;
-
-    public UsersRepositoryInMemoryImpl() {
-        this.users = new ArrayList<>();
-        User user = new User("Vladimir", "qwerty99", LocalDate.parse("1990-08-08"));
-        User user1 = new User("Svetlana", "qwerty98", LocalDate.parse("1991-06-09"));
-        User user2 = new User("Vasiliy", "qwerty97", LocalDate.parse("1992-07-10"));
-
-        users.add(user);
-        users.add(user1);
-        users.add(user2);
-    }
 
     @Override
     public List<User> findAll() {
-        return this.users;
+        return MockStorage.storage().users();
     }
 
     @Override
     public void save(User user) {
-        users.add(user);
+        MockStorage.storage().users().add(user);
+    }
+
+    @Override
+    public boolean isExist(String name, String password) {
+        for (User user : MockStorage.storage().users()) {
+            if (user.getName().equals(name) &&
+                    user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
---
+++ src/main/webapp/jsp/login.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<div class="form-style-2">
+    <div class="form-style-2-heading">
+        Please Sign up!
+    </div>
+    <form method="post" action="login">
+        <label for="name">User name
+            <input class="input-field" type="text" id="name" name="name">
+        </label>
+        <label for="password">Password
+            <input class="input-field" type="password" id="password" name="password">
+        </label>
+        <input type="submit" value="Sign up">
+    </form>
+</div>
+</body>
+</html>
