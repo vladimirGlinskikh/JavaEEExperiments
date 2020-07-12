0aedecec7fb1213bdac9d5ab39b43ef4af2011f5
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-12 14:41:32 +0600

    Remove unsafe string cuts and make a connection through PreparedStatement

--- src/main/java/kz/zhelezyaka/servlets/UsersServlet.java
+++ src/main/java/kz/zhelezyaka/servlets/UsersServlet.java
@@ -7,10 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 @WebServlet("/users")
@@ -47,11 +44,11 @@ public class UsersServlet extends HttpServlet {
         String lastName = req.getParameter("last-name");
 
         try {
-            Statement statement = connection.createStatement();
-            String sqlInsert = "INSERT INTO fix_user(first_name, last_name)" +
-                    "VALUES('" + firstName + "','" + lastName + "');";
-            System.out.println(sqlInsert);
-            statement.execute(sqlInsert);
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO fix_user(first_name, last_name)" +
+                    "VALUES (?, ?)");
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            statement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
         }
