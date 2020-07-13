53a537f2de8274a38c77912e08d27f66668bdf28
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-14 02:04:11 +0600

    Get access to the private field of the class and change it

--- src/main/java/kz/zhelezyaka/reflection/Main.java
+++ src/main/java/kz/zhelezyaka/reflection/Main.java
@@ -3,10 +3,25 @@ package kz.zhelezyaka.reflection;
 import java.lang.reflect.Field;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchFieldException {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         SomeClass someObject = new SomeClass();
         Class<SomeClass> someClass = (Class<SomeClass>) someObject.getClass();
         Field someField = someClass.getField("someField");
         System.out.println(someField.getType());
+
+        Field[] fields = someClass.getFields();
+        for (Field field : fields){
+            System.out.println(field.getType() + " " + field.getName());
+        }
+
+        System.out.println(someObject.someField);
+        someField.set(someObject, 45);
+
+        Field privateField = someClass.getDeclaredField("stringPrivate");
+        System.out.println(privateField);
+
+        privateField.setAccessible(true);
+        privateField.set(someObject, "Vladimir");
+        System.out.println(someObject.getStringPrivate());
     }
 }
--- src/main/java/kz/zhelezyaka/reflection/SomeClass.java
+++ src/main/java/kz/zhelezyaka/reflection/SomeClass.java
@@ -2,4 +2,12 @@ package kz.zhelezyaka.reflection;
 
 public class SomeClass {
     public int someField;
+    public String stringField;
+
+    public String getStringPrivate() {
+        return stringPrivate;
+    }
+
+    private String stringPrivate;
+    public double someDouble;
 }
