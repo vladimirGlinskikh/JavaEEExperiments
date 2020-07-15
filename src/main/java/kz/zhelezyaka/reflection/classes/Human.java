6884496fe9dd59b0264f499026c00bc0048a4fc2
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-15 21:38:42 +0600

    An example of getting class fields through reflection

---
+++ src/main/java/kz/zhelezyaka/reflection/classes/Human.java
@@ -0,0 +1,11 @@
+package kz.zhelezyaka.reflection.classes;
+
+public class Human {
+    public int age;
+    public String name;
+
+    public Human() {
+        this.age = 32;
+        this.name = "DEFAULT_NAME";
+    }
+}
